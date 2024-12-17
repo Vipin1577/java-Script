06d45b8ca54faa58c5a98511d50b8bdd4592e07f
Vipin kanaujiya <154745664+Vipin1577@users.noreply.github.com>
2024-12-17 16:32:00 +0000

    done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,33 @@
+const accountName = 7380913074 // not change
+let accountEmail = "vipinkanaujiya@gmail.com"
+var accountPassword = "123654"
+accountCity ="Mumbai"
+let country;
+
+
+
+
+// //accountName = 321456 // not allwoed
+
+accountEmail = "vipinchaudhari@gmail.com"
+accountPassword = "789456321"
+accountCity  = "Pune"
+
+console.table([accountName,accountEmail,accountPassword ,accountCity,country]);
+
+/*
+prefer not to use var
+because of issse in block scope and functional scope
+
+
+*/
+
+// practice solves
+const NameID = "Bsc CS"
+
+
+console.log(NameID);
+
+
+
+
