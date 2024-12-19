ef5e816579ec86573fa157072a1c5b40dbe83b43
Vipin kanaujiya <154745664+Vipin1577@users.noreply.github.com>
2024-12-19 14:24:44 +0000

    done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+let name ="Vipin kanaujiya "  // String datatype
+let age = 20 // number
+let isLoggedIn = false // boolean
+let state;
+
+
+//   number => 2 to power 53
+//   bigint 
+//   String => ""
+//   boolean => true or false
+//  null  => standalone value
+// undefined => 
+
+// symbol=> unique 
+
+// object
+
+
+console.log(typeof age )
+console.log(typeof null)
+console.log (typeof name);
+
+
+ 
