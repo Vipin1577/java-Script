fe58d89dcd58f1356f043d246b1c605dd0425e7b
Vipin kanaujiya <154745664+Vipin1577@users.noreply.github.com>
2024-12-20 14:56:50 +0000

    coversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let  score = "45"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+
+// "45"
+// "45abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=>  true; 0=> false
+//  ""=> false
+// "vipin" => true
+
+
+
+let  fruits = 33
+let stringNumber =String(fruits)
+console.log(stringNumber)
+console.log( typeof stringNumber);
+
+
+// self 
+
+let a = 33
+ let string= String(a)
+ console.log(string)
+ console.log(typeof string)
+
+let  b = 55;
+let str = String(b)
+console.log(str)
+console.log(typeof str)
